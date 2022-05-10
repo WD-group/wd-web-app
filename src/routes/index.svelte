11c444ab839a7551c6ef960cfdb03b0a8a885980
Giorgio Rengucci <4483986@studenti.unige.it>
2022-05-10 20:38:15 +0200

    sveltekit init

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Button from '@smui/button';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button variant="raised">Default</Button>
+<Button variant="raised" color="secondary">Default</Button>
