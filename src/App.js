a3bd0e28d5b6d5c5e6340df2c0f9420e1dc00a28
Giorgio Rengucci <4483986@studenti.unige.it>
2022-05-21 09:42:01 +0200

    added theme

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import Card from './mycard';
 import Navbar from './mybar';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { blue, green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#651FFF',
+    },
+    secondary: {
+      main: green[500],
+    }
+  }
+})
+
 
 function App() {
   return (
     <>
+      <ThemeProvider theme={theme}>
       <Navbar />
-      <Card 
+      <Card
         image="http://www.campingbellavista.nl/wp-content/uploads/beigua3-8.jpg"
         title="Il Monte Beigua"
         text="Il monte Beigua è il monte più montuoso della Liguria"
         alt="Monte Beigua"
-      />
-      <Card 
+        />
+      <Card
         image="https://www.trekkingways.it/wp-content/uploads/53761314_1158544924306983_4707143552071630848_o.jpg"
         title="Maïonęysē"
         text="Sul monte Beigua si coltiva anche la Maïonęysē. Qui se ne possono assaporare le bellezze, ma la vista è una delle più belle del mondo. Il monte è una delle pietre preziose che si possono trovare in tutto il paese."
-      />
-      <Card 
+        />
+      <Card
         image="https://live.staticflickr.com/561/18394527933_2be5291094_b.jpg"
         title="Il Monte di Cimbera"
         text="Il monte di Cimbera è una cima, non centra nulla con il monte Beigua ma come si può vedere ha un bellissimo borgo in stile barocco."
         alt="Monte di Cimbera"
-      />
+        />
       <Card image="https://img2.juzaphoto.com/002/shared_files/uploads/2293128_l.jpg"
         title="Sempre il monte Beigua"
         text="Il minimo che si può fare è salutare il monte Beigua. Non è una cosa che si può fare in una settimana ma è una cosa che si può fare in un mese."
-      />
-      <Card image="http://images6.fanpop.com/image/photos/35200000/Dog-dogs-35247719-3706-2480.jpg"/>
+        />
+      <Card image="http://images6.fanpop.com/image/photos/35200000/Dog-dogs-35247719-3706-2480.jpg" />
+      </ThemeProvider>
     </>
   );
 }
