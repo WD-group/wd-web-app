90ca3f49588c164e8a5176d3b38e17229b9f1156
Giorgio Rengucci <4483986@studenti.unige.it>
2022-05-21 11:34:36 +0200

    initial commit hero card

---
+++ src/components/hero/hero.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Paper } from '@mui/material';
+
+export default function ActionAreaCard(props) {
+	let text = "testo mancante";
+	return (
+		<Paper elevation={0} style={{ backgroundColor: "#6200EE", padding: "1rem", borderRadius: "0"}}>
+			<CardMedia
+				component="img"
+				//height="140"
+				image="/big_logo.svg"
+				alt=""
+			/>
+		<CardContent>
+				<Typography variant="h6" color="white" align='center'>
+					<br />
+					Il Monte Beigua è in pericolo.<br/>
+					La salute di tutta la popolazione ligure è in pericolo.<br /><br />
+					Organizziamo una serie di Flash Mob per sensibilizzare le persone sul tema, così da evitare che per
+					l'interesse di pochi i più finiscano per correggere il loro caffè con l'amianto.
+				</Typography>
+			</CardContent>
+		</Paper>
+	);
+}
