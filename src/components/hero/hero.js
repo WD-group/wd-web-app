12d175f8f01b28f40d2fdc87c8a6b44033428b91
Giorgio Rengucci <4483986@studenti.unige.it>
2022-07-06 19:18:48 +0200

    removed unused prop

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -5,7 +5,6 @@ import Typography from '@mui/material/Typography';
 import { Paper } from '@mui/material';
 
 export default function ActionAreaCard(props) {
-	let text = "testo mancante";
 	return (
 		<Paper elevation={0} style={{ backgroundColor: "#6D3EBB", padding: "16px", borderRadius: "0", paddingTop:"32px"}}>
 			<CardMedia
