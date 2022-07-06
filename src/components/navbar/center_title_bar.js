6293e4ffdc2c62d2d9b1b8648a16a670b4249c01
Giorgio Rengucci <4483986@studenti.unige.it>
2022-07-06 19:18:04 +0200

    modified name

--- src/components/navbar/center_title_bar.js
+++ src/components/navbar/center_title_bar.js
@@ -3,24 +3,23 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 
 export default function ElevateAppBar(props) {
+	let title = "Titolo Mancante";
 	return (
 		<React.Fragment>
 			<CssBaseline />
 			<AppBar>
 				<Toolbar>
-
-					<img src="/icon.svg" alt="beiguaLogo" className={"navbar-logo-shifted"} sx={{ flexGrow: 1, mt: "16px" }} />
-					<Typography position="absolute" left="0" right="0" variant="poppinstitle" textAlign="center" component="div" sx={{ mr: "auto", ml: "auto", flexGrow: 1 }}>
-						Flash Mob
+					<Link to="/">
+						<img src="/icon.svg" alt="home" className={"navbar-logo-shifted"} sx={{ flexGrow: 1, mt: "16px" }} />
+					</Link>
+					<Typography position="absolute" ml="40%" variant="poppinstitle" textAlign="center" component="div" >
+						{props.title ? props.title : title}
 					</Typography>
-					{/* <div sx={{ flexGrow: 1 }}></div> */}
-
 				</Toolbar>
 			</AppBar>
-			{/* </ElevationScroll> */}
-			<Toolbar />
 		</React.Fragment>
 	);
 }
