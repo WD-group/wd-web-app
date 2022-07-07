a3f9d0b5a84196f5cfa19ac4c656aa6d3b9bc978
Giorgio Rengucci <4483986@studenti.unige.it>
2022-07-07 15:49:30 +0200

    sahdoiu

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,8 +3,20 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Link } from 'react-router-dom';
+import { Button, Typography } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton'
+import CookieIcon from '@mui/icons-material/Cookie';
+import CloseIcon from '@mui/icons-material/Close';
+import Collapse from '@mui/material/Collapse';
+
+
+
 
 export default function ElevateAppBar() {
+	
+	const [open, setOpen] = React.useState(true);
+
 	return (
 		<React.Fragment>
 			<CssBaseline />
@@ -13,6 +25,31 @@ export default function ElevateAppBar() {
 					<Link to="/">
 						<img src="/big_logo.svg" alt="beiguaLogo" className={"navbar-logo-left"} href="/"/>
 					</Link>
+
+
+
+					<Collapse in={open} className='cookie-infos'>
+						<Alert
+							color="primary"
+							icon={false}
+							action={
+								<IconButton
+									aria-label="close"
+									color="primary"
+									size="small"
+									onClick={() => {
+										setOpen(false);
+									}}
+								>
+									<CloseIcon fontSize="inherit" />
+								</IconButton>
+							}
+							sx={{ mb: 2 }}
+						>
+							Questo sito utilizza Cookie.
+						</Alert>
+					</Collapse>
+					
 				</Toolbar>
 			</AppBar>
 		</React.Fragment>
